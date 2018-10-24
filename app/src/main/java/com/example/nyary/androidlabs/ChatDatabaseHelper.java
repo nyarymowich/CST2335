5b5a6c63c931a2dac7b48fa861c0e02cf4498388
nyarymowich <nyarymowich@hotmail.com>
2018-10-24 12:04:52 -0400

    Lab 5 done

---
+++ app/src/main/java/com/example/nyary/androidlabs/ChatDatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.example.nyary.androidlabs;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+
+    static String DATABASE_NAME = "Messages.db";
+    static int VERSION_NUM = 4;
+    final static String KEY_ID = "Id";
+    final static String KEY_MESSAGE = "Message";
+    final static String TABLE_NAME = "Log";
+
+    public ChatDatabaseHelper(Context ctx){
+        super(ctx,DATABASE_NAME,null,VERSION_NUM);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+        db.execSQL("CREATE TABLE " + TABLE_NAME +"("+ KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT , "+ KEY_MESSAGE +" text);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + oldVersion + " newVersion=" + newVersion);
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
+    }
+}
