21834f0596fa7b55aa277f1ac2a6fde2f55a8e6d
nyarymowich <nyarymowich@hotmail.com>
2018-10-29 16:05:57 -0400

    add favorite activity

---
+++ app/src/main/java/com/example/nyary/androidlabs/FoodFavorites.java
@@ -0,0 +1,13 @@
+package com.example.nyary.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodFavorites extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_favorites);
+    }
+}
