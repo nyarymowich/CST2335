b66da47b86aac55b72e8a401312d9ab26f3fd236
nyarymowich <nyarymowich@hotmail.com>
2018-10-31 20:35:41 -0400

    Still stuck

--- app/src/main/java/com/example/nyary/androidlabs/WeatherForecast.java
+++ app/src/main/java/com/example/nyary/androidlabs/WeatherForecast.java
@@ -4,13 +4,21 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Xml;
 import android.view.View;
 import android.widget.ProgressBar;
 
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.util.List;
 
 public class WeatherForecast extends Activity {
 
@@ -28,20 +36,58 @@ public class WeatherForecast extends Activity {
         String max;
         String wind;
         Bitmap picture;
+        XmlPullParser parser;
 
         @Override
         protected String doInBackground(String... strings) {
-            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=d99666875e0e51521f0040a3d97d0f6a&mode=xml&units=metric");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            URL url = null;
+            try {
+                url = new URL("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=d99666875e0e51521f0040a3d97d0f6a&mode=xml&units=metric");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection conn = null;
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             conn.setReadTimeout(10000 /* milliseconds */);
             conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
+            try {
+                conn.setRequestMethod("GET");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
             conn.setDoInput(true);
             // Starts the query
-            conn.connect();
-            return conn.getInputStream();
-        }
+            try {
+                conn.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                parser = parse(conn.getInputStream());
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+            return parser.getAttributeValue(null,"speed");
+
+        }
+        public XmlPullParser parse(InputStream in) throws XmlPullParserException, IOException {
+            try {
+                XmlPullParser parser = Xml.newPullParser();
+                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                parser.setInput(in, null);
+                parser.nextTag();
+                return parser;
+            } finally {
+                in.close();
+            }
+        }
     }
     }
 
